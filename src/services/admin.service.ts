4ea4839db87dbd6e8c7d73fa9727048628631c8a
Adler Castro <adlercastro01@gmail.com>
2025-05-20 23:20:51 -0300

    :sparkles: feat: MVC dos administradores

---
+++ src/services/admin.service.ts
@@ -0,0 +1,15 @@
+import { adminModel } from '@/models/admin.model';
+import { RegisterSchemaType } from '@/schemas/register.schema';
+import { UpdateSchemaType } from '@/schemas/update.schema';
+
+export const adminService = {
+  create: async ({ name, email, password_hash, role }: RegisterSchemaType) =>
+    await adminModel.create({ name, email, password_hash, role }),
+
+  update: async (
+    id: string,
+    { name, email, password_hash, role }: UpdateSchemaType,
+  ) => {
+    await adminModel.update(id, { name, email, password_hash, role });
+  },
+};
