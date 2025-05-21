3cfd37464d6c98d626a2d7ba2625b9a475234fa4
Adler Castro <adlercastro01@gmail.com>
2025-05-21 03:11:23 -0300

    :recycle: refactor: Limpando a tipagem ao dividir o tipo do usuário no JWT

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,3 +1,4 @@
+import { User } from '@/types/user.type';
 import { JWT } from 'fastify-jwt';
 
 declare module 'fastify' {
@@ -8,13 +9,8 @@ declare module 'fastify' {
     authenticate: import('fastify').RouteHandlerMethod;
   }
 }
-type UserPayload = {
-  id: string;
-  email: string;
-  name: string;
-};
 declare module '@fastify/jwt' {
   interface FastifyJWT {
-    user: UserPayload;
+    user: User;
   }
 }
