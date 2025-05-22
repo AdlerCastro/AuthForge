01f553ccacede620732aee6374a1695611d3d092
Adler Castro <adlercastro01@gmail.com>
2025-05-22 14:10:05 -0300

    :sparkles: feat: Adição de respostas para cada caso

--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -2,10 +2,24 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 export function checkRole(required: 'USER' | 'ADMIN') {
   return async function (req: FastifyRequest, res: FastifyReply) {
+    if (req.user.role === 'ADMIN') {
+      return res.status(200).send({
+        success: true,
+        message: 'Acesso concedido',
+      });
+    }
+
+    if (req.user.role === 'USER') {
+      return res.status(403).send({
+        success: true,
+        message: 'Acesso negado. Privilégios insuficientes',
+      });
+    }
+
     if (req.user.role !== required) {
       return res.status(403).send({
         success: false,
-        message: 'Acesso negado: privilégio insuficiente',
+        message: 'Você não é usuário ou administrador',
       });
     }
   };
