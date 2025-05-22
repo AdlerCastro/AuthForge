9c92b8e6816ec66acaaad5094833a69ef484b0a0
Adler Castro <adlercastro01@gmail.com>
2025-05-22 14:09:52 -0300

    :sparkles: feat: Adição de respostas para cada caso

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -1,21 +1,22 @@
-import { FastifyJWT } from '@fastify/jwt';
-import { FastifyRequest, FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest } from 'fastify';
 
 export async function Authenticate(req: FastifyRequest, res: FastifyReply) {
-  const token = req.cookies.access_token;
-
-  if (!token) {
-    return res.status(401).send({
-      success: false,
-      message: 'Token não encontrado',
-    });
-  }
   try {
-    const decoded = req.jwt.verify<FastifyJWT['user']>(token);
+    const token = req.cookies.access_token;
+
+    if (!token) {
+      return res.status(401).send({
+        success: false,
+        message: 'Token não encontrado',
+      });
+    }
+
+    const decoded = req.jwt.verify(token);
     req.user = decoded;
   } catch (error) {
-    return res
-      .status(401)
-      .send({ success: false, message: 'Token inválido. Error ' + error });
+    return res.status(401).send({
+      success: false,
+      message: `Token inválido ou expirado. Error: ${error}`,
+    });
   }
 }
