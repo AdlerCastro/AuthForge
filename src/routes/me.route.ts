eed6b845458dc09a3ced4f84538ab1aeced37af8
Adler Castro <adlercastro01@gmail.com>
2025-05-22 22:37:39 -0300

    :bug: fix: A rota /me nao estava protegida pelo middleware de autenticação e o retorno não era compatível com o schema

--- src/routes/me.route.ts
+++ src/routes/me.route.ts
@@ -1,19 +1,18 @@
 import { meController } from '@/controllers/me.controller';
 import { errorResponseSchema } from '@/schemas/response.schema';
-import { UserSchema } from '@/schemas/user.schema';
+import { PublicUserResponseSchema } from '@/schemas/user.schema';
 import { FastifyTypeInstance } from '@/types/fastifyInstance.type';
 
 export async function meRoute(app: FastifyTypeInstance) {
   app.get(
     '/me',
     {
+      preHandler: [app.authenticate],
       schema: {
         tags: ['me'],
         description: 'Get current user',
         response: {
-          200: UserSchema.omit({ password_hash: true }).describe(
-            'Get current user success',
-          ),
+          200: PublicUserResponseSchema.describe('Get current user success'),
           401: errorResponseSchema,
         },
       },
