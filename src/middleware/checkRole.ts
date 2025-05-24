42eb2822f7a35a086dba0e88ad6e1afb11b3c421
Adler Castro <adlercastro01@gmail.com>
2025-05-24 19:00:31 -0300

    :bug: fix: as respostas do middleware encerravam requisições necessárias

--- src/middleware/checkRole.ts
+++ src/middleware/checkRole.ts
@@ -2,13 +2,6 @@ import { FastifyReply, FastifyRequest } from 'fastify';
 
 export function checkRole(required: 'USER' | 'ADMIN') {
   return async function (req: FastifyRequest, res: FastifyReply) {
-    if (req.user.role === 'ADMIN') {
-      return res.status(200).send({
-        success: true,
-        message: 'Acesso concedido',
-      });
-    }
-
     if (req.user.role === 'USER') {
       return res.status(403).send({
         success: true,
