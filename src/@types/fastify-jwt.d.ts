162c6a4ace60a1589dd3dc9ace6a92d1c69dae8d
Adler Castro <adlercastro01@gmail.com>
2025-07-15 18:27:39 -0300

    :bug: fix: update user type in fastify-jwt definitions to use UserPayload interface

--- src/@types/fastify-jwt.d.ts
+++ src/@types/fastify-jwt.d.ts
@@ -1,4 +1,4 @@
-import { User } from '@/types/user.type';
+import { UserPayload } from '@/core/types/userPayload';
 import { JWT } from 'fastify-jwt';
 
 declare module 'fastify' {
@@ -11,6 +11,6 @@ declare module 'fastify' {
 }
 declare module '@fastify/jwt' {
   interface FastifyJWT {
-    user: User;
+    user: UserPayload;
   }
 }
